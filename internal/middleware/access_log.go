8547411d5caa8779603119317a71dfc556dc5107
强子 <441280451@qq.com>
2021-01-04 09:57:48 +0800

    增加访问日志中间件

--- internal/middleware/access_log.go
+++ internal/middleware/access_log.go
@@ -2,6 +2,9 @@ package middleware
 
 import (
 	"bytes"
+	"github.com/go-programming-tour-book/blog-service/global"
+	"github.com/go-programming-tour-book/blog-service/pkg/logger"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,4 +20,26 @@ func (w AccessLogWriter) Write(p []byte) (int, error) {
 	}
 
 	return w.ResponseWriter.Write(p)
+}
+
+func AccessLog() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		bodyWriter := &AccessLogWriter{body: bytes.NewBufferString(""),
+			ResponseWriter: c.Writer}
+		c.Writer = bodyWriter
+		beginTime := time.Now().Unix()
+		c.Next()
+		endTime := time.Now().Unix()
+
+		fields := logger.Fields{
+			"request":  c.Request.PostForm.Encode(),
+			"response": bodyWriter.body.String(),
+		}
+		global.Logger.WithFields(fields).Infof(
+			"access log: method: %s, status_code: %d, begin_time: %d, end_time: %d",
+			c.Request.Method,
+			bodyWriter.Status(),
+			beginTime,
+			endTime)
+	}
 }
